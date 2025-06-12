6e9a4370c034e2343f1595fa4d387fb3d7c57d73
Ahmad Muzakkir Bin Ismail <50354321+zkkr-ism@users.noreply.github.com>
2025-06-12 18:04:03 +0000

    adding navbar and logo

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
+  imports: [RouterLink, RouterOutlet]
 })
 export class Navbar {
   protected title = 'abstract';
